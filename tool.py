4feccb39f931f50c1c9bd4974ad82c8b12429d86
Tanky Woo <wtq1990@gmail.com>
2013-06-25 13:25:19 +0800

    tool.py

--- tool.py
+++ tool.py
@@ -11,20 +11,27 @@ import markdown2
 from os import path as osp
 from articles import articles
 
-DEBUG = False
-
 MDPATH = './tkwiki'  # The path put markdown files
 HTMLPATH = './html/tkwiki'  # The path put the generated html files
 TPLPATH = './html/template/markdown.tpl'  # The template file path
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 1:
         print('>>ERROR: ', 'Need a markdown file')
         sys.exit(1)
     _md = sys.argv[1]
     dir_name = _md.split('/')[-2]
     md_name = _md.split('/')[-1]
+    if len(sys.argv) == 3:
+        if sys.argv[2] == '--debug':
+            debug = True
+        else:
+            print('>>ERROR: ', 'The third argv should only be `--debug`')
+            sys.exit(1)
+    else:
+        debug = False
+
     try:
         title = articles[dir_name][md_name.split('.')[0]]['title']
     except Exception:
@@ -40,8 +47,8 @@ if __name__ == '__main__':
     html = re.sub('{{ content }}', content, unicode(tpl_html, 'utf-8'))
     html = re.sub('{{ title }}', title, html)
     html = html.encode('utf-8')
-    html_path = osp.join(HTMLPATH, dir_name, md_name.split('.')[0]+'.html2')
-    if DEBUG:
+    html_path = osp.join(HTMLPATH, dir_name, md_name.split('.')[0]+'.html')
+    if debug:
         print(html)
     else:
         with open(html_path, 'w') as wfd:
