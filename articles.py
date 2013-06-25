8b85e79539bd7e1f331481dd5a948bb02cd4fe4a
TankyWoo <wtq1990@gmail.com>
2013-06-25 09:28:23 +0800

    add articles

---
+++ articles.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# wutq@2013-06-25
+
+articles = {
+    'linux' : {
+        'bash_shortcuts' : {
+            'title' : 'Bash Shortcuts',
+        },
+        'linux_commands' : {
+            'title' : 'Linux Commands',
+        },
+        'linux_tricks' : {
+            'title' : 'Linux Tricks',
+        },
+        'shell' : {
+            'title' : 'Shell',
+        },
+        'server_performance' : {
+            'title' : 'Server Peformance',
+        },
+        'ubuntu_network' : {
+            'title' : 'Ubuntu Network',
+        },
+        'uid' : {
+            'title' : 'RUID/EUID/SUID',
+        },
+        'x-windows' : {
+            'title' : 'X Windows安装',
+        },
+    },
+
+    'tool' : {
+        'tcpdump' : {
+            'title' : 'Tcpdump',
+         },
+        'ss' : {
+            'title' : 'ss',
+         },
+        'netstat' : {
+            'title' : 'netstat',
+         },
+        'route' : {
+            'title' : 'route',
+         },
+        'nmap' : {
+            'title' : 'Nmap',
+         },
+        'nc' : {
+            'title' : 'nc(Netcat)',
+         },
+        'curl' : {
+            'title' : 'cUrl',
+         },
+        'iptables' : {
+            'title' : 'iptables',
+         },
+        'ipset' : {
+            'title' : 'ipset',
+         },
+        'mount' : {
+            'title' : 'mount',
+         },
+        'find' : {
+            'title' : 'find',
+         },
+        'top' : {
+            'title' : 'top',
+         },
+        'ps' : {
+            'title' : 'ps',
+         },
+        'iftop' : {
+            'title' : 'iftop',
+         },
+        'git' : {
+            'title' : 'Git',
+         },
+        'svn' : {
+            'title' : 'Svn',
+         },
+        'grep' : {
+            'title' : 'grep',
+         },
+        'sed' : {
+            'title' : 'sed',
+         },
+        'awk' : {
+            'title' : 'awk',
+         },
+        'uniq' : {
+            'title' : 'uniq',
+         },
+        'ssh' : {
+            'title' : 'ssh',
+         },
+        'vim' : {
+            'title' : 'vim',
+         },
+        'vimwiki' : {
+            'title' : 'vimwiki',
+         },
+        'markdown' : {
+            'title' : 'markdown',
+         },
+        'efficiency_tools_i_use' : {
+            'title' : '我使用的效率工具',
+         },
+
+    },
+
+    'software' : {
+        'screen' : {
+            'title' : 'Screen',
+         },
+        'tmux' : {
+            'title' : 'Tmux',
+         },
+        'vsftpd' : {
+            'title' : 'vsftpd',
+         },
+        'samba' : {
+            'title' : 'samba',
+         },
+        'nginx' : {
+            'title' : 'nginx',
+         },
+        'fastcgi' : {
+            'title' : 'fastcgi',
+         },
+        'mysql' : {
+            'title' : 'mysql',
+         },
+        'mongodb' : {
+            'title' : 'mongodb',
+         },
+        'rrdtool' : {
+            'title' : 'rrdtool',
+         },
+        'msmtp_mutt' : {
+            'title' : 'msmtp + mutt',
+         },
+        'moinmoin' : {
+            'title' : 'moinmoin',
+         },
+
+    },
+
+    'cpp' : {
+        'linux_c_akaedu' : {
+            'title' : '《Linux C 编程一站式学习》笔记',
+        },
+    },
+
+    'python' : {
+        'pdb' : {
+            'title' : 'Python Debug',
+        },
+        'argparse' : {
+            'title' : 'argparse',
+        },
+        'classmethod_staticmethod' : {
+            'title' : 'ClassMethod & StaticMethod',
+        },
+        'python_thread' : {
+            'title' : 'Python 进程/线程',
+        },
+        'python_find_source' : {
+            'title' : 'How to Find the Source Code',
+        },
+    },
+
+    'project' : {
+        'passtk' : {
+            'title' : 'Passtk',
+        },
+    },
+
+}
+
