f45a2b1a6d36fd41f58bf4e13ecf9579158322df
TankyWoo <wtq1990@gmail.com>
2013-06-25 09:30:40 +0800

    tool.py

--- html/template/markdown.tpl
+++ html/template/markdown.tpl
@@ -6,6 +6,8 @@
 		<meta name="keywords" content="TankyWoo, Wiki, vimwiki, linux, python, cpp"/>
 		<meta name="description" content="A wiki about Linux, Python, and so on"/>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <a href="https://github.com/tankywoo/wiki"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>
 	</head>
 	<body>
 		<div id="header">
@@ -17,14 +19,17 @@
 		</div>
 		<div class="clearfix">
 		</div>
+		<div id="title">{{ title }}</div>
 		
 		<div id="content">
-		{{ content }}
+
+{{ content }}
+
 		</div>
 
 		<div id="footer">
 			<span class="">Copyright © 2012-2013 <a href="http://www.wutianqi.com/" target="_blank">Tanky Woo</a> 
-				Powered by <a href="http://www.vim.org/" target="_blank">Vim</a> and <a href="http://code.google.com/p/vimwiki/" target="_blank">Vimwiki</a>
+				Powered by <a href="http://www.vim.org/" target="_blank">Vim</a> and <a href="http://daringfireball.net/projects/markdown/" target="_blank">Markdown</a>
 			</span>
 		</div>
 	</body>
--- tool.py
+++ tool.py
@@ -8,25 +8,32 @@ import sys
 import re
 import markdown2
 
+from os import path as osp
+
 
 # The global 
-MDPATH = ''  # The path put markdown files
-HTMLPATH = ''  # The path put the generated html files
-TPLPATH = ''  # The template file path
+MDPATH = './tkwiki'  # The path put markdown files
+HTMLPATH = './html/tkwiki'  # The path put the generated html files
+TPLPATH = './html/template/markdown.tpl'  # The template file path
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('>>ERROR: ', '')
+        print('>>ERROR: ', 'Need a markdown file')
         sys.exit(1)
-    md_file = sys.argv[1]
+    _md = sys.argv[1]
+    dir_name = _md.split('/')[-2]
+    md_name = _md.split('/')[-1]
     md_suffix = ['md', 'mkd', 'markdown']
-    if md_file.split('.')[1] not in md_suffix:
-        print('>>ERROR: ', '')
+    if '.' not in md_name or md_name.split('.')[1] not in md_suffix:
+        print('>>ERROR: ', 'Suffix is error')
         sys.exit(1)
 
-    html = markdown2.markdown_path('/tmp/tmux.markdown')
-    tpl = open('markdown.tpl', 'r')
+    html = markdown2.markdown_path(_md)
+    tpl = open(TPLPATH, 'r')
     tpl_html = ''.join(tpl.readlines())
     newhtml = re.sub('{{ content }}', html, unicode(tpl_html, 'utf-8'))
     newhtml = newhtml.encode('utf-8')
+    html_path = osp.join(HTMLPATH, dir_name, md_name.split('.')[0]+'.html2')
+    with open(html_path, 'w') as wfd:
+        wfd.write(newhtml)
