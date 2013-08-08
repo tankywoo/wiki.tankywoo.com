643eb8f7f2c0ac77991223e4e224df51292d2215
TankyWoo <wtq1990@gmail.com>
2013-08-09 06:17:40 +0800

    update the directory list page

--- tool.py
+++ tool.py
@@ -39,7 +39,7 @@ import markdown2
 from os import path as osp
 
 __author__  = 'Tanky Woo <me@tankywoo.com>'
-__version__ = "1.0"
+__version__ = "1.1"
 __license__ = "MIT License"
 
 BASE_DIR = osp.dirname(osp.realpath(__file__))
@@ -52,10 +52,10 @@ DIR_WIKI = osp.join(BASE_DIR, WIKI_NAME) # markdown wiki file
 DIR_HTML = osp.join(BASE_DIR, "html/%s" % WIKI_NAME) # generrated html file
 DIR_TPL = osp.join(BASE_DIR, "html/template/markdown.tpl") # html template
 
-def _check_md_file_exists(md_file):
-    """Check if the md_file exists."""
-    if not osp.exists(md_file):
-        sys.exit("%s does not exists" % md_file)
+def _check_file_exists(file_):
+    """Check if the file exists."""
+    if not osp.exists(file_):
+        sys.exit("%s does not exists" % file_)
 
 def _check_suffix(md_file):
     """Check if the md_file's suffix is right.
@@ -104,17 +104,55 @@ def _get_title(md_file):
 
         return title
 
-def _md2html(md_file):
+def _md2html(md_file, title):
     """Generate the html from md file, and embed it in html template"""
     content = markdown2.markdown_path(md_file)
     tpl = open(DIR_TPL, "r")
     tpl_html = "".join(tpl.readlines())
     html = re.sub("{{ content }}", content, unicode(tpl_html, "utf-8"))
-    title = _get_title(md_file)
     html = re.sub("{{ title }}", unicode(title, "utf-8"), html)
     html = html.encode("utf-8")
 
     return html
+
+def _update_dir_page(dir_name, md_name, title):
+    """dir_page is the directory page to list the wiki(html)'s link
+
+    A <li> label's syntax is:
+
+        <li><a href="../`dir_name`/`md_name`.html">`title`</a></li>
+
+    Insert the new <li>..</li> in the first line of the <li> lists. Make sure 
+    the top wiki is the newest.
+
+    TODO:
+        * if dir_page_file not exists, create it.
+        * if there is blanklines between <li>, delete it.
+        * if there is blanklinks out of <ul>..</ul>, delete it.
+        * is it necessary to add tab or 4 spaces before ever <li>?
+    """
+    dir_page_file = osp.join(DIR_HTML, dir_name+".html")
+    _check_file_exists(dir_page_file)
+
+    with open(dir_page_file, "rb+") as fd:
+        li_list = fd.readlines()[1:-1]  # ignore the <ul>..</ul> label
+        new_li = "<li><a href=\"../%s/%s.html\">%s</a></li>" % \
+                (dir_name, md_name, title)
+
+        # if new_li exists, ignore; otherwise insert it in the first <li> list
+        for li in li_list:
+            if new_li in li:
+                return
+        fd.seek(0) # set current position to the beginning of the file
+        li_list.insert(0, "\t%s\n"%new_li)
+        new_content = "<ul>\n" + "".join(li_list) + "</ul>\n"
+        fd.write(new_content)
+
+def _update_wiki_page(dir_name, md_name):
+    html_path = osp.join(DIR_HTML, dir_name, md_name+".html")
+    with open(html_path, "w") as wfd:
+        wfd.write(html)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
@@ -129,17 +167,16 @@ if __name__ == "__main__":
     md_file = osp.realpath(parse_args.md_file)
     debug_mode = parse_args.debug
 
-    _check_md_file_exists(md_file)
-
+    _check_file_exists(md_file)
     _check_suffix(md_file)
 
     dir_name, md_name = _get_dir_and_md_name(md_file)
+    title = _get_title(md_file)
+    html = _md2html(md_file, title)
 
-    html = _md2html(md_file)
+    _update_dir_page(dir_name, md_name, title)
 
     if debug_mode:
         print(html)
     else:
-        html_path = osp.join(DIR_HTML, dir_name, md_name+".html")
-        with open(html_path, "w") as wfd:
-            wfd.write(html)
+        _update_wiki_page(dir_name, md_name)
