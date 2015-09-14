953dec26f8d700b0ed95313fe4eefacd8b582682
Tanky Woo <wtq1990@gmail.com>
2015-09-14 23:27:24 +0800

    Update fabfile and _config.yml

--- fabfile.py
+++ fabfile.py
@@ -1,85 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import print_function, absolute_import, with_statement
 
 import os
-import os.path
-from sys import exit
-import datetime
-from fabric.api import env, local, run
+import sys
+from fabric.api import env, local, task, settings
 from fabric.colors import blue, red
 import fabric.contrib.project as project
+from simiki import config
 
-# Remote host and username
-env.hosts = ["192.81.133.96"]
-env.user = "root"
+# XXX must run fab in root path of wiki
+configs = config.parse_config('_config.yml')
+
 env.colorize_errors = True
 
-# Local output path
-env.local_output = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)),
-    "output/")
-# Remote path to deploy output
-env.remote_output = "/opt/wiki.tankywoo.com/output"
 
-# Other options
-env.rsync_delete = False
+def do_exit(msg):
+    print(red(msg))
+    print(blue('Exit!'))
+    sys.exit()
 
-def update_simiki():
-    print(blue("Old Version: "))
-    run("simiki -V")
-    run("pip install -U simiki")
-    print(blue("New Version: "))
-    run("simiki -V")
 
-def deploy():
-    if not env.remote_output:
-        if env.rsync_delete:
-            print(red("You can't enable env.rsync_delete option "
-                "if env.remote_output is not set!!!"))
-            print(blue("Exit"))
-            exit()
+def get_rsync_configs():
+    if 'deploy' in configs:
+        for item in configs['deploy']:
+            if item['type'] == 'rsync':
+                return item
+    return None
 
-        print(red("Warning: env.remote_output directory is not set!\n"
-                    "This will cause some problems!!!"))
-        ans = raw_input(red("Do you want to continue? (y/N) "))
-        if ans != "y":
-            print(blue("Exit"))
-            exit()
+# cannot put this block in deploy_rsync() for env.hosts
+rsync_configs = get_rsync_configs()
+if rsync_configs:
+    env.user = rsync_configs.get('user', 'root')
+    # Remote host and username
+    if 'host' not in rsync_configs:
+        do_exit('Warning: rsync host not set in _config.yml!')
+    env.hosts = [rsync_configs['host'],]
 
+    # Local output path
+    env.local_output = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        configs['destination'])
+
+    # Remote path to deploy output
+    if 'dir' not in rsync_configs:
+        do_exit('Warning: rsync dir not set in _config.yml!')
+    env.remote_output = rsync_configs['dir']
+
+    # Other options
+    env.port = rsync_configs.get('port')
+    env.rsync_delete = rsync_configs.get('delete', False)
+
+
+def deploy_rsync(deploy_configs):
+    '''for rsync'''
     project.rsync_project(
-        local_dir = env.local_output,
-        remote_dir = env.remote_output.rstrip("/") + "/",
-        delete = env.rsync_delete
+        local_dir=env.local_output.rstrip("/")+"/",
+        remote_dir=env.remote_output.rstrip("/")+"/",
+        delete=env.rsync_delete
     )
 
-def g():
-    local("simiki generate")
 
-def p():
-    local("simiki preview")
+def deploy_git(deploy_configs):
+    '''for pages service of such as github/gitcafe ...'''
+    with settings(warn_only=True):
+        res = local('which -s ghp-import; echo $?', capture=True)
+        if int(res.strip()):
+            do_exit('Warning: ghp-import not installed! '
+                    'run: `pip install ghp-import`')
+    output_dir = configs['destination']
+    remote = deploy_configs.get('remote', 'origin')
+    branch = deploy_configs.get('branch', 'gh-pages')
+    # commit gh-pages branch and push to remote
+    _mesg = 'Update output documentation'
+    local('ghp-import -p -m "{0}" -r {1} -b {2} {3}' \
+          .format(_mesg, remote, branch, output_dir))
 
-def gp():
-    g()
-    p()
 
-def commit(commit_file=None, message=None):
-    '''Auto commit tracked changes to git
-    
-    Usage:
-        fab commit - commit all modified tracked files
-        fab commit:[commit_file=]'file1 file2 fil3' - commit specified files
-        fab commit:[commit_file=]'file1 file2 file3',[message=]'your commit message' - commit specified files with custom commit message
-        fab commit:message='your commit message' - commit all modified tracked files with custom commit message
-    '''
-    if not message:
-        message = 'Update {0}'.format(datetime.datetime.now()
-                                      .strftime("%Y-%m-%d %H:%M:%S"))
-    # add all tracked files
-    # TODO change . to content/ ?
-    if not commit_file:
-        commit_file = '-u .'
+@task
+def deploy():
+    '''deploy your output to server/git'''
+    if 'deploy' not in configs or not isinstance(configs['deploy'], list):
+        do_exit('Warning: deploy not set right in _config.yml')
 
-    local("git add {0}".format(commit_file))
-    local("git commit -m '{0}'".format(message))
+    deploy_configs = configs['deploy']
+
+    done = False
+
+    for deploy_item in deploy_configs:
+        deploy_type = deploy_item.pop('type')
+        func_name = 'deploy_{0}'.format(deploy_type)
+        func = globals().get(func_name)
+        if not func:
+            do_exit('Warning: not supprt {0} deploy method'.format(deploy_type))
+        func(deploy_item)
+        done = True
+
+    if not done:
+        print(blue('do nothing...'))
+
+
+@task
+def commit():
+    '''git commit source changes from all tracked/untracked files'''
+    message = 'Update Documentation'
+    commit_file = '-A'  # include tracked and untracked files
+
+    with settings(warn_only=True):
+        # Changes not staged for commit
+        res = local('git status --porcelain 2>/dev/null | grep "^ M" | wc -l',
+                    capture=True)
+        if int(res.strip()):
+            local("git add {0}".format(commit_file))
+
+        # Changes to be committed
+        res = local('git status --porcelain 2>/dev/null | grep "^M" | wc -l',
+                    capture=True)
+        if int(res.strip()):
+            local("git commit -m '{0}'".format(message))
